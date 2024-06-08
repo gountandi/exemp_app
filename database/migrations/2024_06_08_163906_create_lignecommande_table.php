fb169ef806c519cd7d477b6b32e02d1f78b3ab38
nabila <ngountandi@icloud.com>
2024-06-08 17:34:30 +0000

    new migation

--- database/migrations/2024_06_08_163906_create_lignecommande_table.php
+++ database/migrations/2024_06_08_163906_create_lignecommande_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('lignecommande', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('quantiter');
         });
     }
 
