79d92b709e6b3bbaedbb35af00230347ffc26aee
nabila <ngountandi@icloud.com>
2024-06-08 16:57:05 +0000

    new migration

---
+++ database/migrations/2024_06_08_163756_create_evenement_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evenement', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('country');
+            $table->string('town');
+            $table->string('adresse');
+            $table->string('gps');
+            $table->string('description');
+            $table->date('date');
+            $table->string('heure_debut');
+            $table->string('heur_fin');
+            $table->integer('entreprise_id');
+            $table->foreign('entreprise_id')->references('id')->on('entreprise');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evenement');
+    }
+};
